8aaee323f2b4fef02728a047202efc1ea08c80e0
Adrian De Lisle <adrian.delisle@hootsuite.com>
2015-01-30 16:36:09 -0800

    add duration logging

--- tasks/casper_queues.js
+++ tasks/casper_queues.js
@@ -13,6 +13,8 @@ var async = require('async');
 var path = require('path');
 var fs = require('fs');
 var _ = require('underscore');
+var duration = require('duration');
+
 var casperBin = "node_modules/casperjs/bin/casperjs";
 
 var casperRun = function (file, xunit, args) {
@@ -22,8 +24,8 @@ var casperRun = function (file, xunit, args) {
   args.unshift("test");
   args.push(file);
   args.push(xunit);
-  console.log(casperBin);
-  console.log(args);
+  //console.log(casperBin);
+  //console.log(args);
 
   if (!fs.existsSync(casperBin)) {
     console.log("where's casper?");
@@ -40,6 +42,7 @@ var casperRun = function (file, xunit, args) {
 
 module.exports = function (grunt) {
   grunt.registerMultiTask('casper_queues', 'Run casperjs tests in the cli in parallel queues', function () {
+    var startTime = new Date();
     var options = this.options();
     var queueConfig = options.queue;
     var args = options.args;
@@ -47,21 +50,27 @@ module.exports = function (grunt) {
 
     var queue = async.queue(function (task, callback) {
       console.log("Starting queue: ", task.name);
+      var taskStartTime = new Date();
 
       task.tests.forEach(function (test) {
         var result = casperRun(test.file, test.xunit, args.slice()); //args.slice() makes a copy of the args for casperRun to modify and use
         console.log(result);
+        console.log(queue.running());
       });
 
+      console.log("Test set: " + task.name + " took " + new duration(taskStartTime).toString("%Ss.%Ls") + " seconds to run.");
       callback(); //required for queue task to be considered done
-    }, 1);
+    }, 5);
 
     queue.drain = function () {
-      console.log("Everything is done yo");
+      console.log("Everything is done yo. It took " + new duration(startTime).toString("%Ss.%Ls") + " seconds to run.");
     };
 
+    queue.pause();
+
+    console.log(queue.saturated);
     _.each(queueConfig, function (tasks, name) {
-      console.log("Pushing: ", name, tasks);
+      //console.log("Pushing: ", name, tasks);
       queue.push({'name': name, 'tests': tasks}, function (err) {
         if (err) {
           console.log("Error processing queue", name);
@@ -70,5 +79,7 @@ module.exports = function (grunt) {
         }
       });
     });
+
+    queue.resume();
   });
 };
