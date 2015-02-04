c9fe9460a5d8076fc7007067cc395d677c4d853d
Adrian De Lisle <adriandelisle@gmail.com>
2015-02-04 00:30:48 -0800

    added the ability to do X rounds of retrying failed tests from the previous round

--- tasks/casper_queues.js
+++ tasks/casper_queues.js
@@ -17,23 +17,24 @@ var duration = require('duration');
 
 var casperBin = "node_modules/casperjs/bin/casperjs";
 
-var args = [];
-
-var casperRun = function (test, callback) {
-  var testArgs = args.slice()
+var casperRun = function (test, args, failed, callback) {
   var file = path.resolve(process.cwd(), test.file);
   var xunit = "--xunit=" + path.resolve(process.cwd(), test.xunit);
   
-  testArgs.unshift("test");
-  testArgs.unshift(casperBin);
-  testArgs.push(file);
-  testArgs.push(xunit);
+  args.unshift("test");
+  args.unshift(casperBin);
+  args.push(file);
+  args.push(xunit);
 
-  var command = testArgs.join(" ");
+  var command = args.join(" ");
 
   console.log(command);
 
-  var result = exec(command, {encoding: "utf8", timeout: 30000}, function (error, stdout, stderr) {
+  var result = exec(command, {encoding: "utf8"}, function (error, stdout, stderr) {
+    if (error) {
+      failed.push(test);
+      console.log(error);
+    }
     console.log("stdout: ", stdout);
     console.log("stderr: ", stderr);
     callback();
@@ -45,36 +46,65 @@ module.exports = function (grunt) {
     var startTime = new Date();
     var options = this.options();
     var queueConfig = options.queue;
-    args = options.args;
+    var maxRetries = options.maxRetries || 1;
+    var retries = 0;
+    var args = options.args;
     var done = this.async();
+    var failedTasks = {};
+    
+    var pushToQueue = function (queue, queueConfig) {
+      _.each(queueConfig, function (tasks, name) {
+        queue.push({'name': name, 'tests': tasks}, function (err) {
+          if (err) {
+            console.log("Error processing queue", name);
+          } else {
+            console.log("Finished queue: ", name);
+          }
+        });
+      });
+    }
 
     var queue = async.queue(function (task, callback) {
-      console.log("Starting queue: ", task.name);
+      if (retries) {
+        console.log("Retrying failed tests in test set: ", task.name);
+      }
+      console.log("Starting set: ", task.name);
       var taskStartTime = new Date();
-      
-      async.eachSeries(task.tests, casperRun, function (err) {
+      var failedTests = [];
+
+      async.eachSeries(task.tests, function (test, callback) {
+        casperRun(test, args.slice(), failedTests, callback);
+      }, function (err) {
         if (err) {
           console.log("Something when wrong: ", err);
         } else {
           console.log("Test set: " + task.name + " took " + new duration(taskStartTime).toString("%Ss.%Ls") + " seconds to run.");
+          if (failedTests.length >= 1) {
+            failedTasks[task.name] = failedTests;
+          }
         }
         callback(); //required for queue task to be considered done
       });
     }, 2);
 
     queue.drain = function () {
-      console.log("Everything is done yo. It took " + new duration(startTime).toString("%Ss.%Ls") + " seconds to run.");
-      done();
+      if (_.isEmpty(failedTasks) && retries === 0) {
+        console.log("Everything is done yo. It took " + new duration(startTime).toString("%Ss.%Ls") + " seconds to run.");
+        done();
+      } else if (_.isEmpty(failedTasks)) { 
+        console.log("Everything is done yo, all tests pasted a retrying some " + retries + " times. It took " + new duration(startTime).toString("%Ss.%Ls") + " seconds to run.");
+        done();
+      } else if (!_.isEmpty(failedTasks) && retries >= maxRetries) {
+        console.log("Everything is done yo, but some tests failed. It took " + new duration(startTime).toString("%Ss.%Ls") + " seconds to run.");
+        console.log("Failed Tests: ", failedTasks);
+        done();
+      } else {
+        retries++;
+        pushToQueue(queue, failedTasks);
+        failedTasks = {};
+      }
     };
 
-    _.each(queueConfig, function (tasks, name) {
-      queue.push({'name': name, 'tests': tasks}, function (err) {
-        if (err) {
-          console.log("Error processing queue", name);
-        } else {
-          console.log("Finished queue: ", name);
-        }
-      });
-    });
+    pushToQueue(queue, queueConfig);
   });
 };
