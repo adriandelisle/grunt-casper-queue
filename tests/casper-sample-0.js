d699bbb2a4d5ff883d11149813cd25397b83adda
Gabriel Sagula <gabriel.sagula@hootsuite.com>
2015-10-13 16:08:46 -0700

    Updated release version. Old version is not compatible anymore.

--- tests/casper-sample-0.js
+++ tests/casper-sample-0.js
@@ -2,7 +2,7 @@
 casper.test.begin('Google search retrieves 10 or more results', 2, function suite (test) {
     casper.start('http://www.google.fr/', function () {
         test.assertExists('form[action="/search"]', 'main form is found');
-        this.fill('form[action="/searchi"]', {
+        this.fill('form[action="/search"]', {
             q: 'casperjs'
         }, true);
     });
